9c9030f0b7feeb87f20528cc8a180ff9cd7e4f5e
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-31 15:24:11 -0300

    :sparkles: feat: Removendo funcionário "João" da tabela

---
+++ src/main/java/main/funcionalidades/RemovendoFuncionario.java
@@ -0,0 +1,25 @@
+package main.funcionalidades;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import main.conexao.ConexaoDB;
+
+public class RemovendoFuncionario {
+	public static void remover() throws SQLException {
+		Connection connection = ConexaoDB.conectar();
+		String query = "DELETE FROM funcionarios WHERE nome = ?";
+		PreparedStatement pstm = connection.prepareStatement(query);
+		pstm.setString(1, "João");
+
+		int contador = pstm.executeUpdate();
+
+		if (contador > 0) {
+			System.out.println("Pessoa(s) Excluída(s) com sucesso!");
+			System.out.println("Linhas afetadas: " + contador);
+		}
+
+		connection.close();
+	}
+}
