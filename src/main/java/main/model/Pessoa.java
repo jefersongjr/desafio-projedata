61186783e8ec4b31ca0df55f7470287672432788
jefersongjr <jeferson_gjr@hotmail.com>
2023-01-31 14:40:33 -0300

    :sparkles: criando a Classe Pessoa

---
+++ src/main/java/main/model/Pessoa.java
@@ -0,0 +1,49 @@
+package main.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Pessoa {
+
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected int id;
+	protected String nome;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataDeNascimento;	
+
+	public Pessoa(String nome, LocalDate dataDeNascimento2) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dataDeNascimento = dataDeNascimento2;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+}
